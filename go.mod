33fe51a3c7a5135366de93580f92f6f45beb54f9
黎大胆 <1151904797@qq.com>
2026-09-04 22:39:49 +0800

    save

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/call
+
+go 1.27.0
+
+require github.com/Torontototokyo/go-add v0.0.0-20260904143213-6bce72510e5b // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	Add "github.com/Torontototokyo/go-add"
+)
+
+func main() {
+	c := Add.Add(10, 10)
+	fmt.Println(c)
+}
